94497f8cbdae568a47f2389e566098cdb05a670e
Connor Lee <connorlee976@gmail.com>
2025-04-24 23:12:11 -0400

    starting word2vec

--- models/word2vec.py
+++ models/word2vec.py
@@ -1,4 +1,13 @@
+import numpy as np
+
 from gensim.models import KeyedVectors
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 # using the Google News Word2Vec model (already pretrained and better because we don't have much time)
 model = KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin", binary=True)
+
+stop_words = set(stopwords.words("english"))
+
+def vector_convert(overview, model):
+    words = word_tokenize(overview)
